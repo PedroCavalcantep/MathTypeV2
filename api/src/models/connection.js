0faf8785e2d3b0fda60f188f1e1c5747f21b6c60
Pedro Cavalcante <dinho.cavalcantep@gmail.com>
2024-04-20 15:06:50 -0300

    model e controller de scores criado

--- api/src/models/connection.js
+++
@@ -1,9 +0,0 @@
-// const mysql = require("mysql2/promise");
-const pg = require("pg");
-require("dotenv").config();
-const { Pool } = pg;
-
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
-});
-module.exports = pool;
--- api/src/server.js
+++
@@ -1,6 +0,0 @@
-const app = require("./app.js");
-// eslint-disable-next-line no-unused-vars
-const dotenv = require("dotenv").config();
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
