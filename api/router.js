81a12a25daa039c774f9de560a628f64026f8931
Josias <josiasjr2020@gmail.com>
2024-04-23 18:43:46 -0300

    commit feito pra o meu vs code largar de choro

--- api/router.js
+++ api/router.js
@@ -5,7 +5,6 @@ const router = express.Router();
 const userController = require("./src/controllers/userController");
 const scoreController = require("./src/controllers/scoreController");
 
-<<<<<<< Updated upstream
 router.get("/User", userController.listAll);
 
 router.post("/User", userController.createUser);
@@ -14,9 +13,8 @@ router.delete("/deleteUser/:id", userController.deleteUser);
 
 router.get("/User/:id", userController.getUser);
 
-router.post("/userLogin",  userController.loginUser);
-=======
+router.post("/userLogin", userController.loginUser);
+
 router.get("/Score", scoreController.listAll);
->>>>>>> Stashed changes
 
 module.exports = router;
