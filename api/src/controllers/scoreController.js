c6a4be44e149278aaa830551129f123626dfe68f
Josias <josiasjr2020@gmail.com>
2024-04-24 01:20:21 -0300

    scoremodels com trycatch

--- api/src/controllers/scoreController.js
+++ api/src/controllers/scoreController.js
@@ -1,8 +1,12 @@
 const scoreModels = require("../models/scoreModels");
 
 const listAll = async (req, res) => {
-    const score = await scoreModels.listAll();
-    return res.status(200).json({score});
+    try {
+        const score = await scoreModels.listAll();
+        return res.status(200).json({score});
+    } catch (err) {
+        return res.status(500).json({message: "Internal server error"})
+    }   
 };
 
 module.exports = {
