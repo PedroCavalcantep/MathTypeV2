8b750fd55989c9661575b76b1a3e540cae45f2a0
Josia <josiasjr2020@gmail.com>
2024-04-30 17:18:30 -0300

    atualizacao de rotas de score

--- api/router.js
+++ api/router.js
@@ -5,16 +5,26 @@ const router = express.Router();
 const userController = require("./src/controllers/userController");
 const scoreController = require("./src/controllers/scoreController");
 
-router.get("/User", userController.listAll);
+// rotas user 
 
-router.post("/User", userController.createUser);
+router.get("/user", userController.listAll);
+
+router.post("/user", userController.createUser);
 
 router.delete("/deleteUser/:id", userController.deleteUser);
 
-router.get("/User/:id", userController.getUser);
+router.get("/user/:id", userController.getUser);
 
 router.post("/userLogin", userController.loginUser);
 
-router.get("/Score", scoreController.listAll);
+// rotas score
+
+router.get("/score", scoreController.listAll);
+
+router.get("/topScore", scoreController.topScore);
+
+router.post("/score", scoreController.createScore);
+
+router.delete("/deleteScore/:id", scoreController.deleteScore);
 
 module.exports = router;
