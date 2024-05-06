3a1acc776daceb9c6f7885adeeae40a5fa848815
Kauebarbosa-dev <123207805+Kauebarbosa-dev@users.noreply.github.com>
2024-05-05 21:55:31 -0300

    Card Game
    Create a Card Game

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,22 @@
 import Image from "next/image";
 import MainPage from "./components/MainPage";
+import CardGame from "./components/CardGame";
+import Navbar from "./components/header";
 
 export default function Home() {
   return (    
+    
+    <div>
+
+    <div>
+    <Navbar></Navbar>
     <MainPage></MainPage>
+    </div>
+
+    <div className="flex  justify-center py-10 px-8 ">
+      <CardGame></CardGame>
+    </div>
+
+    </div>
   );
 }
