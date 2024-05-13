224c23e3f0b34c97c7334beb1f82326891ab040b
PedroCavalcanteP <dinho.cavalcantep@gmail.com>
2024-05-13 15:14:07 -0300

    login popup

--- src/app/components/Login.js
+++ src/app/components/Login.js
@@ -1,36 +1,23 @@
-import Image from "next/image";
-import Close from '../assets/close_icon.svg';
+import Image from "next/image"
+import Close from "../assets/close_icon.svg"
 
+export default function Login({ isOpen, closeForm }) {
+	if (isOpen) {
+		return (
+			<div
+				className=" fixed inset-0 bg-black bg-opacity-25
+      backdrop-blur-sm flex justify-center items-center text-white "
+			>
+				<div className="flex flex-col w-[600px] ">
+					<button onClick={closeForm} className=" place-self-end">
+						<Image src={Close} alt="Close" />
+					</button>
 
-export default function Login() {
-
-
-  const Login = ({ isVisible}) => {
-  if (isVisible) return null;
-
-
-  return (
-    <div className=" fixed inset-0 bg-black bg-opacity-25
-    backdrop-blur-sm flex justify-center items-center text-white ">
-      
-    <div className="flex flex-col w-[600px] ">
-
-
-        <button className=" place-self-end">
-        <Image 
-          src={Close}
-          alt="Close"
-        />
-        </button>
-
-        <div className="bg-[#333A45]  p-2">
-            Login
-        </div>
-
-    </div>
-
-
-    </div>
-   )
-  }
+					<div className="bg-[#333A45]  p-2">Login</div>
+				</div>
+			</div>
+		)
+	} else {
+		return null
+	}
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,20 @@
-import Image from "next/image";
-import MainPage from "./components/MainPage";
-import CardGame from "./components/CardGame";
-import Navbar from "./components/header";
-import Login from "./components/Login";
+import Image from "next/image"
+import MainPage from "./components/MainPage"
+import CardGame from "./components/CardGame"
+import Navbar from "./components/header"
+import Login from "./components/Login"
 
 export default function Home() {
-  return (    
-    
-    <div>
+	return (
+		<div>
+			<div>
+				<Navbar></Navbar>
+				<MainPage></MainPage>
+			</div>
 
-    <div>
-    <Navbar></Navbar>
-    <MainPage></MainPage>
-    </div>
-
-    <div className="flex  justify-center py-10 px-8 ">
-      <CardGame></CardGame>
-    </div>
-
-    <Login></Login>
-    </div>
-  );
+			<div className="flex  justify-center py-10 px-8 ">
+				<CardGame></CardGame>
+			</div>
+		</div>
+	)
 }
